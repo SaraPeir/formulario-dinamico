41343a1248597fd00305ecc9386ddb5c5e68527d
SaraPeir <sara.peirce1987@gmail.com>
2019-04-18 23:30:30 +0200

    clean up of styles, use of boopstrap

--- js/createForm.js
+++ js/createForm.js
@@ -1,33 +1,34 @@
 'use strict'
 
-let formsContainer = document.querySelector('.forms-container');
+let formsContainer = document.querySelector('#forms-container');
 
 function createForm(array){
     let form = document.createElement("form");
-    form.setAttribute("class", "form-container border border-primary");
+    form.setAttribute("class", "col-8 border border-primary bg-light py-4 my-4");
     let inputContainer = document.createElement("div");
+    inputContainer.setAttribute('class', 'row justify-content-center');
     let formButton = document.createElement("button");
     formButton.setAttribute('type', 'submit');
-    formButton.setAttribute('class', 'btn btn-primary');
+    formButton.setAttribute('class', 'btn btn-primary col-6');
     formButton.innerHTML = 'Submit';
 
     formsContainer.appendChild(form);
     form.appendChild(inputContainer);
-    form.appendChild(formButton);
 
     let inputs;
     if(array.length > 0) {
         for(let i = 0; i < array.length; i++){
             inputs = array.map(obj => 
-                `<input class="input-style" type="${obj.type}"
+                `<input class="mb-3 col-10" type="${obj.type}"
                     name="${obj.name}"
                    id="${obj.id}"
-                   placeholder="${obj.name}"
+                   placeholder="${obj.name}${obj.required ? "*" : ''}"
                    ${obj.required ? "required" : ''}
             >
             </input>`).join('');
         }
         inputContainer.innerHTML = inputs;
+        inputContainer.appendChild(formButton);
     }
     reset();
     array.length = 0;
