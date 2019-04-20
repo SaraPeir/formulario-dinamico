9ff3787d13a039447e89208f05961e30e5625045
SaraPeir <sara.peirce1987@gmail.com>
2019-04-20 22:03:40 +0200

    added local storage

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 'use strict'
 
 let formsContainer = document.querySelector('#forms-container');
-
 let x = document.querySelectorAll("#configuration-single-field > .checkbox-input");
 let y = document.querySelectorAll("#configuration-single-field > .checkbox-input-isRequired");
 let requiredFieldText = document.querySelectorAll("#configuration-single-field > #required-field");
 
 let array = [];
+!localStorage.formArray && localStorage.setItem('formArray', JSON.stringify([]));
 
 function selectCheckbox(array=[], i) {
     if (x[i].checked) {
@@ -39,7 +39,6 @@ function selectRequiredField(array=[], i) {
     }
     console.log('array', array);
 }
-
 
 function getArray(){
     console.log('getArray', array);
@@ -82,8 +81,50 @@ function createForm(array, callback){
 
     let inputs;
     if(array.length > 0) {
-        for(let i = 0; i < array.length; i++){
-            inputs = array.map(obj => 
+        inputs = array.map(obj => 
+            `<input class="mb-3 col-10" type="${obj.type}"
+                name="${obj.name}"
+                id="${obj.id}"
+                placeholder="${obj.name}${obj.required ? "*" : ''}"
+                ${obj.required ? "required" : ''}
+        >
+        </input>`).join('');
+        inputContainer.innerHTML = inputs;
+        inputContainer.appendChild(formButton);
+
+        let storageArray = JSON.parse(localStorage.getItem('formArray'))
+        
+        storageArray && storageArray.push(array);
+        console.log('storageArray', storageArray);
+        localStorage.setItem('formArray', JSON.stringify(storageArray));
+        
+        callback && callback();
+        array.length = 0;
+    } 
+}
+
+function triggerEvent(array, callback) {
+    return array.length > 0 && createForm(array, callback); 
+}
+
+function createFormsFromStorage(){
+    const storageArray = localStorage.formArray && JSON.parse(localStorage.getItem('formArray'));
+    let inputs;
+    if(storageArray && storageArray.length > 0) {
+        for(let i = 0; i < storageArray.length; i++){
+            let form = document.createElement("form");
+            form.setAttribute("class", "col-8 border border-primary bg-light py-4 my-4");
+            let inputContainer = document.createElement("div");
+            inputContainer.setAttribute('class', 'row justify-content-center');
+            let formButton = document.createElement("button");
+            formButton.setAttribute('type', 'submit');
+            formButton.setAttribute('class', 'btn btn-primary col-6');
+            formButton.innerHTML = 'Submit';
+
+            formsContainer.appendChild(form);
+            form.appendChild(inputContainer);
+
+            inputs = storageArray[i].map(obj => 
                 `<input class="mb-3 col-10" type="${obj.type}"
                     name="${obj.name}"
                    id="${obj.id}"
@@ -91,15 +132,11 @@ function createForm(array, callback){
                    ${obj.required ? "required" : ''}
             >
             </input>`).join('');
+
+            inputContainer.innerHTML = inputs;
+            inputContainer.appendChild(formButton);
         }
-        inputContainer.innerHTML = inputs;
-        inputContainer.appendChild(formButton);
-    }
-
-    callback && callback();
-    array.length = 0;
+    } 
 }
 
-function triggerEvent(array, callback) {
-    return array.length > 0 && createForm(array, callback); 
-}
+createFormsFromStorage();
